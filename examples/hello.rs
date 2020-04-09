fb0480fe838e494a1fd28dc6600bafc076e692ac
Ian Kettlewell <ian.kettlewell@gmail.com>
2020-04-08 17:52:57 -0700

    Fix up hello example

--- examples/hello.rs
+++ examples/hello.rs
@@ -21,26 +21,8 @@ fn main() {
     let gl = glow::Context::from_loader_function(|s| gl_context.get_proc_address(s));
 
     event_loop.run(move |event| match event {
-        Event::WindowMoved { .. } => {
-            println!("EVENT: {:?}", event);
-        }
-        Event::KeyDown { key } => match key {
-            Key::A => {
-                app.set_cursor(Cursor::IBeam);
-            }
-            Key::B => {
-                app.set_cursor(Cursor::Arrow);
-            }
-            _ => {}
-        },
         Event::Draw { .. } => {
-            //  gl_context.set_window(Some(&window.id)).unwrap();
-
-            // Make the GLContext current to the thread that this callback runs on.
-            //gl_context.make_current();
-
             // Clear the screen to a lovely shade of blue.
-
             unsafe {
                 gl.clear_color(0.3765, 0.3137, 0.8627, 1.0);
                 gl.clear(COLOR_BUFFER_BIT | DEPTH_BUFFER_BIT);
@@ -51,9 +33,6 @@ fn main() {
             // It is not necessary for this example,
             // but calling request_frame ensures the program redraws continuously.
             window.request_redraw();
-
-            // println!("{}", now.elapsed().as_millis());
-            // now = std::time::Instant::now();
         }
         _ => {}
     });
