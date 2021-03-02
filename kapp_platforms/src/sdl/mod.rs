8b52a9820e343402f48544aaf37c54e4e8170e12
Ian Kettlewell <ian.kettlewell@gmail.com>
2021-03-02 00:35:37 -0500

    Further implementation of platform function calls

--- kapp_platforms/src/sdl/mod.rs
+++ kapp_platforms/src/sdl/mod.rs
@@ -3,17 +3,28 @@ pub mod prelude {
     pub use kapp_platform_common::*;
 }
 
-use fermium::{events::*, video::*, *};
+use fermium::{events::*, keyboard::*, mouse::*, rect::*, stdinc::*, video::*, *};
 use kapp_platform_common::*;
+use std::ffi::CString;
 
-pub struct PlatformApplication {}
+pub struct PlatformApplication {
+    // These cursors are deallocated with `SDL_FreeCursor` in PlatformApplication's Drop
+    arrow_cursor: *mut SDL_Cursor,
+    ibeam_cursor: *mut SDL_Cursor,
+    open_hand_cursor: *mut SDL_Cursor,
+}
 
 impl PlatformApplicationTrait for PlatformApplication {
     type EventLoop = PlatformEventLoop;
     fn new() -> Self {
         unsafe {
             assert!(SDL_Init(SDL_INIT_EVERYTHING) == 0);
-            Self {}
+
+            Self {
+                arrow_cursor: SDL_CreateSystemCursor(SDL_SYSTEM_CURSOR_ARROW),
+                ibeam_cursor: SDL_CreateSystemCursor(SDL_SYSTEM_CURSOR_IBEAM),
+                open_hand_cursor: SDL_CreateSystemCursor(SDL_SYSTEM_CURSOR_IBEAM),
+            }
         }
     }
 
@@ -21,27 +32,91 @@ impl PlatformApplicationTrait for PlatformApplication {
         PlatformEventLoop {}
     }
 
-    fn set_window_position(&mut self, _window_id: WindowId, _x: u32, _y: u32) {}
-    fn set_window_size(&mut self, _window_id: WindowId, _width: u32, _height: u32) {}
-    fn set_window_title(&mut self, _window_id: WindowId, _title: &str) {}
-    fn minimize_window(&mut self, _window_id: WindowId) {}
-    fn maximize_window(&mut self, _window_id: WindowId) {}
-    fn get_window_size(&mut self, _window_id: WindowId) -> (u32, u32) {
-        unimplemented!()
+    fn set_window_position(&mut self, window_id: WindowId, x: u32, y: u32) {
+        unsafe {
+            SDL_SetWindowPosition(window_id.raw() as *mut SDL_Window, x as i32, y as i32);
+        }
     }
-    fn get_window_scale(&mut self, _window_id: WindowId) -> f64 {
-        unimplemented!()
+    fn set_window_size(&mut self, window_id: WindowId, width: u32, height: u32) {
+        unsafe {
+            SDL_SetWindowPosition(
+                window_id.raw() as *mut SDL_Window,
+                width as i32,
+                height as i32,
+            );
+        }
     }
-    fn fullscreen_window(&mut self, _window_id: WindowId) {}
-    fn restore_window(&mut self, _window_id: WindowId) {
-        unimplemented!()
+    fn set_window_title(&mut self, window_id: WindowId, title: &str) {
+        unsafe {
+            let c_string = CString::new(title).unwrap();
+            SDL_SetWindowTitle(window_id.raw() as *mut SDL_Window, c_string.as_ptr());
+        }
     }
-    fn close_window(&mut self, _window_id: WindowId) {}
-    fn redraw_window(&mut self, _window_id: WindowId) {}
+    fn minimize_window(&mut self, window_id: WindowId) {
+        unsafe {
+            SDL_MinimizeWindow(window_id.raw() as *mut SDL_Window);
+        }
+    }
+    fn maximize_window(&mut self, window_id: WindowId) {
+        unsafe {
+            SDL_MaximizeWindow(window_id.raw() as *mut SDL_Window);
+        }
+    }
+    fn get_window_size(&mut self, window_id: WindowId) -> (u32, u32) {
+        let mut width = 0;
+        let mut height = 0;
+        unsafe {
+            SDL_GetWindowSize(window_id.raw() as *mut SDL_Window, &mut width, &mut height);
+        }
+        (width as u32, height as u32)
+    }
 
-    fn lock_mouse_position(&mut self) {}
+    fn get_window_scale(&mut self, window_id: WindowId) -> f64 {
+        let (logical_width, logical_height) = self.get_window_size(window_id);
+        let mut physical_width = 0;
+        let mut physical_height = 0;
 
-    fn unlock_mouse_position(&mut self) {}
+        // This call returns the actual pixel widths that would be in a framebuffer.
+        unsafe {
+            SDL_GL_GetDrawableSize(
+                window_id.raw() as *mut SDL_Window,
+                &mut physical_width,
+                &mut physical_height,
+            );
+        }
+        logical_width as f64 / physical_width as f64
+    }
+
+    fn fullscreen_window(&mut self, window_id: WindowId) {
+        unsafe {
+            SDL_SetWindowFullscreen(window_id.raw() as *mut SDL_Window, SDL_WINDOW_FULLSCREEN.0);
+        }
+    }
+    fn restore_window(&mut self, window_id: WindowId) {
+        unsafe {
+            SDL_RestoreWindow(window_id.raw() as *mut SDL_Window);
+        }
+    }
+    fn close_window(&mut self, window_id: WindowId) {
+        unsafe {
+            SDL_DestroyWindow(window_id.raw() as *mut SDL_Window);
+        }
+    }
+    fn redraw_window(&mut self, _window_id: WindowId) {
+        // Does nothing on the SDL backend.
+    }
+
+    fn lock_mouse_position(&mut self) {
+        unsafe {
+            SDL_SetRelativeMouseMode(SDL_TRUE);
+        }
+    }
+
+    fn unlock_mouse_position(&mut self) {
+        unsafe {
+            SDL_SetRelativeMouseMode(SDL_FALSE);
+        }
+    }
 
     fn new_window(&mut self, window_parameters: &WindowParameters) -> WindowId {
         let (x, y) = window_parameters.position.unwrap_or((
@@ -56,7 +131,7 @@ impl PlatformApplicationTrait for PlatformApplication {
         // SDL_WINDOW_OPENGL is probably not something `kapp`
         // wants to assume.
         // But this is tolerable for now.
-        let mut flags = (SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI);
+        let mut flags = SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI;
         if window_parameters.resizable {
             flags |= SDL_WINDOW_RESIZABLE;
         }
@@ -69,8 +144,8 @@ impl PlatformApplicationTrait for PlatformApplication {
                 b"demo\0".as_ptr().cast(),
                 x as i32,
                 y as i32,
-                width as i32,
-                height as i32,
+                (width / 2) as i32,
+                (height / 2) as i32,
                 flags.0,
             );
 
@@ -99,27 +174,63 @@ impl PlatformApplicationTrait for PlatformApplication {
         }
     }
 
-    fn set_cursor(&mut self, cursor: Cursor) {}
-    fn hide_cursor(&mut self) {}
-    fn show_cursor(&mut self) {}
+    fn set_cursor(&mut self, cursor: Cursor) {
+        let cursor = match cursor {
+            Cursor::IBeam => self.ibeam_cursor,
+            Cursor::OpenHand => self.open_hand_cursor,
+            _ => self.arrow_cursor,
+        };
+        unsafe {
+            SDL_SetCursor(cursor);
+        }
+    }
 
-    fn raw_window_handle(&self, _window_id: WindowId) -> RawWindowHandle {
+    fn hide_cursor(&mut self) {
+        unsafe {
+            SDL_ShowCursor(SDL_DISABLE);
+        }
+    }
+    fn show_cursor(&mut self) {
+        unsafe {
+            SDL_ShowCursor(SDL_ENABLE);
+        }
+    }
+
+    fn raw_window_handle(&self, window_id: WindowId) -> RawWindowHandle {
+        // TODO: This requires calling SDL_GetWindowWMInfo and placing the data
+        // correctly into RawWindowHandle.
         unimplemented!()
     }
 
-    fn start_text_input(&mut self) {}
+    fn start_text_input(&mut self) {
+        unsafe {
+            SDL_StartTextInput();
+        }
+    }
 
-    fn end_text_input(&mut self) {}
+    fn end_text_input(&mut self) {
+        unsafe {
+            SDL_StopTextInput();
+        }
+    }
 
     fn set_text_input_rectangle(
         &mut self,
         _window_id: WindowId,
-        _x: f64,
-        _y: f64,
-        _width: f64,
-        _height: f64,
+        x: f64,
+        y: f64,
+        width: f64,
+        height: f64,
     ) {
-        // Perhaps a hidden text input field could be moved to make IME input appear in the right place.
+        let mut rectangle = SDL_Rect {
+            x: x as c_int,
+            y: y as c_int,
+            w: width as c_int,
+            h: height as c_int,
+        };
+        unsafe {
+            SDL_SetTextInputRect(&mut rectangle);
+        }
     }
 }
 
@@ -127,6 +238,9 @@ impl PlatformApplicationTrait for PlatformApplication {
 impl Drop for PlatformApplication {
     fn drop(&mut self) {
         unsafe {
+            SDL_FreeCursor(self.arrow_cursor);
+            SDL_FreeCursor(self.ibeam_cursor);
+            SDL_FreeCursor(self.open_hand_cursor);
             SDL_Quit();
         }
     }
@@ -145,7 +259,7 @@ impl PlatformEventLoopTrait for PlatformEventLoop {
                     SDL_QUIT => callback(Event::QuitRequested),
                     SDL_WINDOWEVENT => {
                         let window_event = sdl_event.window;
-                        // Fermium doesn't expose SDL_GetWindowFromID
+                        // TODO: Fermium doesn't expose SDL_GetWindowFromID
                         // let window_id = WindowId::new(SDL_GetWindowFromID(window_event.windowID));
                         let window_id = WindowId::new(std::ptr::null_mut());
                         match window_event.event {
@@ -156,6 +270,15 @@ impl PlatformEventLoopTrait for PlatformEventLoop {
                         }
                     }
                     _ => continue,
+                }
+
+                // If there are no events remaining, we're at the end of the event loop
+                if SDL_HasEvents(SDL_FIRSTEVENT, SDL_LASTEVENT) == SDL_FALSE {
+                    callback(Event::EventsCleared);
+
+                    // TODO: Keep track of all windows and send a draw call to each here.
+                    let window_id = WindowId::new(std::ptr::null_mut());
+                    callback(Event::Draw { window_id });
                 }
             }
         }
