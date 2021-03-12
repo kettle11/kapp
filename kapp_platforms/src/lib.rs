8e385b8c807e0fc5c4fbd49047cbb3f9b3c6fb03
Ian Kettlewell <ian.kettlewell@gmail.com>
2021-03-12 18:48:47 -0500

    Conditional SDL feature

--- kapp_platforms/src/lib.rs
+++ kapp_platforms/src/lib.rs
@@ -1,18 +1,21 @@
-#[cfg(target_os = "macos")]
+#[cfg(all(target_os = "macos", not(feature = "SDL")))]
 mod macos;
-#[cfg(target_os = "macos")]
+#[cfg(all(target_os = "macos", not(feature = "SDL")))]
 pub use macos::*;
-
-#[cfg(target_os = "macos")]
+#[cfg(all(target_os = "macos", not(feature = "SDL")))]
 #[macro_use]
 extern crate objc;
 
-#[cfg(target_arch = "wasm32")]
+#[cfg(all(target_arch = "wasm32", not(feature = "SDL")))]
 mod web;
-#[cfg(target_arch = "wasm32")]
+#[cfg(all(target_arch = "wasm32", not(feature = "SDL")))]
 pub use web::*;
-
-#[cfg(target_os = "windows")]
+#[cfg(all(target_os = "windows", not(feature = "SDL")))]
 mod windows;
-#[cfg(target_os = "windows")]
+#[cfg(all(target_os = "windows", not(feature = "SDL")))]
 pub use windows::*;
+
+#[cfg(feature = "SDL")]
+mod sdl;
+#[cfg(feature = "SDL")]
+pub use sdl::*;
